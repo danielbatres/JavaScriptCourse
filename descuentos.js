d4c026e9e93fcd140c3bdb546da4684396ece89d
danielbatres <danielbatresuser@gmail.com>
2022-05-27 16:36:52 -0600

    funciones mas inteligentes

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,3 @@
-const precioOriginal = 120;
-const descuento = 18;
-
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -8,13 +5,35 @@ function calcularPrecioConDescuento(precio, descuento) {
     return precioConDescuento
 }
 
+const coupons = [
+    "JuanDC_es_Batman",
+    "pero_no_le_digas_a_nadie",
+    "es_un_secreto"
+];
+
 function onClickButtonPriceDiscount() {
     const InputPrice = document.getElementById("inputPrice");
-    const InputDiscount = document.getElementById("inputDiscount")
+    const InputDiscount = document.getElementById("inputDiscount");
+    const InputCoupon = document.getElementById("inputCoupon");
     const priceValue = InputPrice.value;
     const discountValue = InputDiscount.value;
+    const couponValue = InputCoupon.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    let descuento;
+
+    switch(couponValue) {
+        case coupons[0]:
+            descuento = 15;
+        break;
+        case coupons[1]:
+            descuento = 30;
+        break;
+        case coupons[2]:
+            descuento = 25;
+        break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
     const resultPrice = document.getElementById("ResultPrice")
     resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
 }
