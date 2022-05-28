70b3f123b7704b605171be644a30a27b62abfdd2
danielbatres <danielbatresuser@gmail.com>
2022-05-27 20:12:56 -0600

    Function moda

--- moda.js
+++ moda.js
@@ -31,3 +31,29 @@ const lista1Array = Object.entries(lista1Count).sort(
 );
 
 const moda = lista1Array[lista1Array.length - 1];
+
+// Function moda
+
+function calcularModa(lista) {
+    const listaCount = {};
+
+    lista.map(
+        function (elemento) {
+            if (listaCount[elemento]) {
+                listaCount[elemento] += 1;
+            } else {
+                listaCount[elemento] = 1;
+            }
+        }
+    );
+
+    const listaArray = Object.entries(listaCount).sort(
+        function (elementoA, elementoB) {
+            return elementoA[1] - elementoB[1];
+        }
+    );
+
+    const moda = listaArray[listaArray.length - 1];
+
+    return moda;
+}
